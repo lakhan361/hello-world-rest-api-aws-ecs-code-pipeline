00aec148c8c0ad258bcb8bae305d776781a81763
Lakhan <Lakhan@crimsoncloud.in>
2020-02-24 11:06:05 +0530

    pipeline

--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/HelloWorldController.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/HelloWorldController.java
@@ -7,14 +7,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloWorldController {
 
-	@GetMapping(path = "/hello-world")
+	@GetMapping(path = "/hello-world v3")
 	public String helloWorld() {
 		return "Hello World";
 	}
 
 	@GetMapping(path = "/hello-world-bean")
 	public HelloWorldBean helloWorldBean() {
-		return new HelloWorldBean("Hello World - Changed - V2");
+		return new HelloWorldBean("Hello World - Changed - V3");
 	}
 	
 	@GetMapping(path = "/hello-world/path-variable/{name}")
